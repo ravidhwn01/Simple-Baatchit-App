769fc5d037de3dc70277124e62382992994e7e4a
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-08 01:12:58 +0530

    chat app

--- server.js
+++ server.js
@@ -14,15 +14,27 @@ app.get('/',(req,res)=>{
 })
 io.sockets.on("connection",function  (socket){
     connections.push(socket);
-    console.log("connected : %s socket connected", connections.length );
-    socket.on("disconnect", function (data){
-connections.slice(connections.indexOf(socket),1);
-console.log("disconnect : %s socket connected", connections.length)
-    } )
-    socket.on("send message",function (data){
-        console.log(data);
-        io.sockets.emit("new message", {msg:data} )
+    console.log("connected : %s sockets connected",connections.length);
+    socket.on("disconnect",function  (data){
+        users.splice(users.indexOf(socket.username),1)
+        connections.splice(connections.indexOf(socket),1)
+        console.log("disconnected: %s sockets connected",connections.length)
     })
+    socket.on("send message",function  (data){
+        io.sockets.emit("new message",{msg:data,user:socket.username})
+    })
+    socket.on("new user",function  (data,callback){
+        callback(true)
+        socket.username = data
+        users.push(socket.username)
+        updateUsernames()
+    }
+    )
+    function updateUsernames(){
+        io.sockets.emit("get users",users)
+    }
+
+   
 })
 
 console.log("server is listening")
